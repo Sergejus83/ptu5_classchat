94fa99e88adab8ab2631f1ba02462dc7b4daf331
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-24 14:33:51 +0200

    New ampty app's form created

---
+++ ptu5_classchat/classchat/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+# class Post(models.Model):
+#     pass
